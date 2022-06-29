be0cee27372c23c1f124bdfc80974348ac81ecbb
Uzair Navid Iftikhar <uiftikhar@chippercash.com>
2022-06-29 22:16:07 +0500

    Branch out on error

--- main.py
+++ main.py
@@ -78,80 +78,83 @@ if __name__ == "__main__":
 
     image = deskew.deskew(image)
     if image is None:
-        st.write("Not a valid image of a passport")
-    imageForFaceDetection = cv2.resize(
-        image, None, fx=1.75, fy=1.75, interpolation=cv2.INTER_CUBIC
-    )
-    image = cv2.resize(image, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
-
-    face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
-    faces = face_cascade.detectMultiScale(
-        imageForFaceDetection, minSize=(60, 60), scaleFactor=1.1, minNeighbors=9
-    )
-    gray = cv2.cvtColor(imageForFaceDetection, cv2.COLOR_BGR2GRAY)
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(
-            imageForFaceDetection,
-            (int(x - 0.1 * w), int(y - 0.25 * h)),
-            (int(x + 1.1 * w), int(y + 1.25 * h)),
-            (255, 0, 0),
-            2,
+        st.write("Not a valid image of a Nigerian Passport")
+    else:
+        imageForFaceDetection = cv2.resize(
+            image, None, fx=1.75, fy=1.75, interpolation=cv2.INTER_CUBIC
         )
-        roi_gray = gray[y : y + h, x : x + w]
-        roi_color = imageForFaceDetection[y : y + h, x : x + w]
+        image = cv2.resize(image, None, fx=1.5, fy=1.5, interpolation=cv2.INTER_CUBIC)
 
-    st.image(
-        imageForFaceDetection,
-        caption="Face detection result",
-        use_column_width=True,
-        channels="BGR",
-    )
+        face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+        faces = face_cascade.detectMultiScale(
+            imageForFaceDetection, minSize=(60, 60), scaleFactor=1.1, minNeighbors=9
+        )
+        gray = cv2.cvtColor(imageForFaceDetection, cv2.COLOR_BGR2GRAY)
 
-    if image is not None:
-        image = cv2.bilateralFilter(image, 9, 75, 75)
-        thresh = cv2.threshold(image, 130, 255, cv2.THRESH_BINARY)[1]
-        morph_struct = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
-        thresh = cv2.erode(thresh, morph_struct, anchor=(-1, -1), iterations=1)
-        thresh = cv2.dilate(thresh, morph_struct, anchor=(-1, -1), iterations=1)
+        for (x, y, w, h) in faces:
+            cv2.rectangle(
+                imageForFaceDetection,
+                (int(x - 0.1 * w), int(y - 0.25 * h)),
+                (int(x + 1.1 * w), int(y + 1.25 * h)),
+                (255, 0, 0),
+                2,
+            )
+            roi_gray = gray[y : y + h, x : x + w]
+            roi_color = imageForFaceDetection[y : y + h, x : x + w]
 
-        custom_config = r"-c tessedit_char_whitelist='ABCDEÉFGHIJKLMNOPQRSTUVWXYZ/-<1234567890 ' --psm 6 --tessdata-dir ."
-        text = pytesseract.image_to_string(image, config=custom_config, lang="ocrb")
+        st.image(
+            imageForFaceDetection,
+            caption="Face detection result",
+            use_column_width=True,
+            channels="BGR",
+        )
 
-        if len(text) < 30:
-            st.write("MRZ malformed or unreadable.")
-        else:
-            text_split = text.split()
+        if image is not None:
+            image = cv2.bilateralFilter(image, 9, 75, 75)
+            thresh = cv2.threshold(image, 130, 255, cv2.THRESH_BINARY)[1]
+            morph_struct = cv2.getStructuringElement(cv2.MORPH_RECT, (4, 4))
+            thresh = cv2.erode(thresh, morph_struct, anchor=(-1, -1), iterations=1)
+            thresh = cv2.dilate(thresh, morph_struct, anchor=(-1, -1), iterations=1)
 
-            mrz = []
-            for line in text_split:
-                if len(line) > 20 and "<" in line:
-                    mrz.append(line)
-            if len(mrz) < 2:
+            custom_config = r"-c tessedit_char_whitelist='ABCDEÉFGHIJKLMNOPQRSTUVWXYZ/-<1234567890 ' --psm 6 --tessdata-dir ."
+            text = pytesseract.image_to_string(image, config=custom_config, lang="ocrb")
+
+            if len(text) < 30:
                 st.write("MRZ malformed or unreadable.")
             else:
-                mrz_text = "\n".join(mrz)
-                info_dict = read_mrz(mrz_text)
-                if mrz_text:
-                    if mrz_text[0] == "P":
-                        info_dict["document_type"] = "Passport"
-                    info_dict["mrz_text"] = mrz_text
+                text_split = text.split()
 
-            # Looks for the passport number in the entire OCR result, in case it can't be read from the MRZ properly
-            if (
-                "passport_number" not in info_dict
-                or len(info_dict["passport_number"]) < 8
-            ):
-                matches = re.findall(r"([A-C]{1}[0-9]{8})", text, flags=re.MULTILINE)
-                if matches:
-                    info_dict["passport_number"] = matches[0]
+                mrz = []
+                for line in text_split:
+                    if len(line) > 20 and "<" in line:
+                        mrz.append(line)
+                if len(mrz) < 2:
+                    st.write("MRZ malformed or unreadable.")
+                else:
+                    mrz_text = "\n".join(mrz)
+                    info_dict = read_mrz(mrz_text)
+                    if mrz_text:
+                        if mrz_text[0] == "P":
+                            info_dict["document_type"] = "Passport"
+                        info_dict["mrz_text"] = mrz_text
 
-    if info_dict:
-        st.subheader("OCR Results")
-        for key, val in info_dict.items():
-            if type(val) is datetime.datetime:
-                st.markdown("**" + key + "**: " + val.strftime("%dth %B %Y"))
-            else:
-                st.markdown("**" + key + "**: " + val)
-    else:
-        st.subheader("Error: Not a valid image of a Nigerian Passport")
+                # Looks for the passport number in the entire OCR result, in case it can't be read from the MRZ properly
+                if (
+                    "passport_number" not in info_dict
+                    or len(info_dict["passport_number"]) < 8
+                ):
+                    matches = re.findall(
+                        r"([A-C]{1}[0-9]{8})", text, flags=re.MULTILINE
+                    )
+                    if matches:
+                        info_dict["passport_number"] = matches[0]
+
+        if info_dict:
+            st.subheader("OCR Results")
+            for key, val in info_dict.items():
+                if type(val) is datetime.datetime:
+                    st.markdown("**" + key + "**: " + val.strftime("%dth %B %Y"))
+                else:
+                    st.markdown("**" + key + "**: " + val)
+        else:
+            st.subheader("Error: Not a valid image of a Nigerian Passport")
